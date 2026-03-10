eaa07af1fdb6e14c8237d29afacb9680f55b1233
kamau-David <dayvieek@gmail.com>
2026-03-10 20:30:05 +0300

    initial commit

---
+++ models/Product.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    trim: true,
+    maxlength: [50, 'Name cannot be more than 50 characters']
+  },
+  description: {
+    type: String,
+    required: [true, 'Please add a description'],
+    maxlength: [500, 'Description cannot be more than 500 characters']
+  },
+  price: {
+    type: Number,
+    required: [true, 'Please add a price']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
